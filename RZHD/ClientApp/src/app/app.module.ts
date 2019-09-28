f0c448885f01d64e080f2c28fe7deacb973a4520
Unknown <enselerizer@mail.ru>
2019-09-28 12:11:52 +0300

    Add api creating

--- RZHD/ClientApp/src/app/app.module.ts
+++ RZHD/ClientApp/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { ApiModule } from './api/api.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    RouterModule.forRoot([]),
+    FormsModule,
+    ApiModule.forRoot({ rootUrl: "https://rzhd.rtuitlab.ru"})
   ],
   providers: [],
   bootstrap: [AppComponent]
