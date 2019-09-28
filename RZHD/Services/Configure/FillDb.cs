1248a0ae5f2d9f3535a0a8190b9a1baa1be08497
Nick <nikitamm59rus@gmail.com>
2019-09-28 10:17:30 +0300

    add default user + return ci

--- RZHD/Services/Configure/FillDb.cs
+++ RZHD/Services/Configure/FillDb.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using RZHD.Data;
+using RZHD.Models;
+using RZHD.Models.Options;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +14,17 @@ namespace RZHD.Services.Configure
 {
     public class FillDb : IConfigureWork
     {
+        private readonly DatabaseContext context;
+        private readonly UserManager<User> userManager;
+        private readonly FillDbOptions options;
+
+        public FillDb(DatabaseContext context, IOptions<FillDbOptions> options,
+            UserManager<User> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+            this.options = options.Value;
+        }
         public async Task Configure()
         {
             await AddDefaultUser();
@@ -15,7 +32,18 @@ namespace RZHD.Services.Configure
 
         private async Task AddDefaultUser()
         {
-            //throw new NotImplementedException();
+            if (!await userManager.Users.AnyAsync(u => u.Email == options.Email))
+            {
+                await userManager.CreateAsync(new User
+                {
+                    Firstname = options.Firstname,
+                    Secondname = options.Secondname,
+                    Middlename = options.Middlename,
+                    Email = options.Email,
+                    Age = options.Age,
+                    BonusQuantity = options.BonusQuantity
+                }, options.Password);
+            }
         }
     }
 }
